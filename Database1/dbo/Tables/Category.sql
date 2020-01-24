fd3031c26d38eb750cd1407cf934721b2429be8a
Dong <Dong@WMD>
2020-01-24 10:48:37 -0500

    add category table

---
+++ Database1/dbo/Tables/Category.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Category] (
+    [CategoryId]  INT           IDENTITY (1, 1) NOT NULL,
+    [Description] NVARCHAR (20) NOT NULL,
+    PRIMARY KEY CLUSTERED ([CategoryId] ASC)
+);
+
